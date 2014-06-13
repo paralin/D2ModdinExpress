5c87392144e9e9bfcd28ef0f280786aa0b53ebfc
Christian Stewart <kidovate@gmail.com>
2014-06-13 15:36:19 -0700

    Track if users are invited

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 var SteamStrategy = require('passport-steam').Strategy;
 var User = require('../schema/user');
 var Steam = require('steam-webapi');
+var request = require('request');
 Steam.key = process.env.STEAM_KEY||"CBD4B6FF1511AD17841196BCE6B1E3E4";
 Steam.ready(function(err){
   console.log("Steam web api ready.");
@@ -39,18 +40,27 @@ module.exports = function(passport){
           user.profile.name = profile.personaname;
           return done(null, user);
         }else{
-          require('crypto').randomBytes(48, function(ex, buf){
-            var newUser = new User();
-            newUser._id = buf.toString('hex');
-            newUser.steam = profile;
-            newUser.profile.name = profile.personaname;
-            newUser.authItems = [];
-            newUser.save(function(err){
-              if(err)
-                throw err;
-              return done(null, newUser);
+          request('http://d2modd.in/api/getInvitedUserStatus/?key=822956923836&user_id='+profile.steamid, function(rerr, rresp, rbody){
+            if(rerr){
+              return done(rerr, null);
+            }
+            data = JSON.parse(rbody);
+            if(data.invited != 1){
+              return done(new Error("You have not been invited to D2Moddin yet. You are "+data.queue_id+" in the queue."));
+            }
+            require('crypto').randomBytes(12, function(ex, buf){
+              var newUser = new User();
+              newUser._id = buf.toString('hex');
+              newUser.steam = profile;
+              newUser.profile.name = profile.personaname;
+              newUser.authItems = [];
+              newUser.save(function(err){
+                if(err)
+                  throw err;
+                return done(null, newUser);
+              });
             });
-          });
+          })
         }
       })
     });
