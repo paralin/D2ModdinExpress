50f9cde10800cc0f8e2622391f27e04e3c1253f3
Christian Stewart <kidovate@gmail.com>
2014-06-18 14:29:36 -0400

    Fixed

--- local.bash
+++ local.bash
@@ -1,4 +1,4 @@
 export PORT=80
-export ROOT_URL="http://localhost/"
+export ROOT_URL="http://107.170.200.42/"
 export MONGO_URL="mongodb://development:fcnbZAAHb5ajp57WGCWn@kahana.mongohq.com:10065/d2mpdev"
 nodemon app.js
