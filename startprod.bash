64b7746994fc85e99ae3c4a48c484fd30474553e
Christian Stewart <kidovate@gmail.com>
2014-06-23 15:27:52 -0400

    Added startprod

---
+++ startprod.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PORT=80
+export ROOT_URL="http://www.d2modd.in/"
+export MONGO_URL="mongodb://newweb:BMPtShexCYChZ565fm5q@capital.1.mongolayer.com:10048/d2moddin"
+export NODE_ENV="production"
+node /app/app.js
