8230bc759d451e6a27f4590b36c7fd64c6d45364
Christian Stewart <kidovate@gmail.com>
2014-06-24 17:58:12 -0400

    production in prod

--- startprod.bash
+++ startprod.bash
@@ -2,6 +2,6 @@
 export PORT=80
 export ROOT_URL="http://d2modd.in/"
 export MONGO_URL="mongodb://d2mpsys:aucCXtn7d8twSEm4C28G@127.0.0.1/d2moddin"
-export NODE_ENV="production"
+export NODE_ENV=production
 export USE_CLUSTER=true
 node /app/app.js
