91cf4b23718ca7870bab50a866f554ce936e8981
Christian Stewart <kidovate@gmail.com>
2014-07-20 15:11:34 -0400

    Updates

--- startprod.bash
+++ startprod.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PORT=80
+export PORT=8080
 export ROOT_URL="http://d2modd.in/"
 export MONGO_URL="mongodb://d2mpsys:aucCXtn7d8twSEm4C28G@127.0.0.1/d2moddin"
 export NODE_ENV=production
